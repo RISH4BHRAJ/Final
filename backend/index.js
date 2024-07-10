0022218e8da22556c09c307bda02f05921d274e7
RISH4BHRAJ <rishabhraj.meesala2021@vitstudent.ac.in>
2024-07-10 21:35:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://final-two-red.vercel.app/"],
+        origin: [""],
         methods: ["POST", "GET"],
         credentials: true
     }
